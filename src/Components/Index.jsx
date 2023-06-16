a89e0399d510dd69c392fd6f5299199fb8bca4b1
Aimen Ellahi <you@example.com>
2023-06-16 18:06:31 +0500

    fixing loader

--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -13,7 +13,9 @@ import { GEB4 } from "./GEB4";
 const Loader = () => {
   const { progress } = useProgress();
   return (
-    <Html center style={{ color: "black" }}>
+    <Html style={{ color: "black"
+    ,marginTop: "22vw", marginLeft: "48vw"
+     }}>
       {progress} % loaded
     </Html>
   );
@@ -24,7 +26,7 @@ export default function Index() {
 
   return (
     <div style={{ width: "100%", height: "100vh" }}>
-      <Canvas camera={{ position: [0, 0, 0], fov: 37 }}>
+      <Canvas camera={{ position: [0, 0, 0], fov: 37 }} >
         <ambientLight intensity={0.8} />
         <spotLight position={[10, 15, 10]} angle={0.3} />
         <Suspense fallback={<Loader />}>
